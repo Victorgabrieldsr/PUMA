f5446360ad424d2fac9a08d84c52b8457a100119
Victorgabrieldsr <victorgabrieldsr@gmail.com>
2024-04-21 22:57:08 -0300

    responsivo 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,60 @@
 const carousel = document.querySelector('#content-carrosel');
 const prevButton = document.querySelector('.carousel-prev');
 const nextButton = document.querySelector('.carousel-next');
+let isTransitioning = false;
 
 function goToNextSlide() {
-  // Adicionando uma pequena pausa antes de aplicar a transição
-  setTimeout(() => {
-    carousel.style.transition = "transform 0.3s";
-  }, 10);
+  if (isTransitioning) return;
+  isTransitioning = true;
 
-  const firstCard = carousel.firstElementChild;
-  carousel.appendChild(firstCard);
+  const cardWidth = carousel.firstElementChild.offsetWidth;
+
+  carousel.style.transition = 'transform 0.4s';
+  carousel.style.transform = `translateX(-${cardWidth}px)`;
+
+  const transitionEndHandler = () => {
+    carousel.appendChild(carousel.firstElementChild);
+    carousel.style.transition = 'none';
+    carousel.style.transform = 'translateX(0)';
+    isTransitioning = false;
+    carousel.removeEventListener('transitionend', transitionEndHandler);
+  };
+
+  carousel.addEventListener('transitionend', transitionEndHandler);
 }
 
 function goToPrevSlide() {
-  // Adicionando uma pequena pausa antes de aplicar a transição
+  if (isTransitioning) return;
+  isTransitioning = true;
+
+  const cardWidth = carousel.firstElementChild.offsetWidth;
+
+  carousel.style.transition = 'none';
+  carousel.style.transform = `translateX(-${cardWidth}px)`;
+  carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild);
+
   setTimeout(() => {
-    carousel.style.transition = "transform 0.3s";
+    carousel.style.transition = 'transform 0.4s';
+    carousel.style.transform = 'translateX(0)';
   }, 10);
 
-  const lastCard = carousel.lastElementChild;
-  const firstCard = carousel.firstElementChild;
-  carousel.insertBefore(lastCard, firstCard);
+  const transitionEndHandler = () => {
+    isTransitioning = false;
+    carousel.removeEventListener('transitionend', transitionEndHandler);
+  };
+
+  carousel.addEventListener('transitionend', transitionEndHandler);
 }
 
-prevButton.addEventListener('click', () => {
-  goToPrevSlide();
-});
+prevButton.addEventListener('click', goToPrevSlide);
+nextButton.addEventListener('click', goToNextSlide);
 
-nextButton.addEventListener('click', () => {
 
-  goToNextSlide();
-});
+
+let buttonsCarrosel = document.getElementsByTagName("ion-icon");
+for (let i = 0; i < buttonsCarrosel.length; i++) {
+  buttonsCarrosel[i].addEventListener("mouseout", function(){
+    this.style.transition = "0.3s";
+  });
+}
+
